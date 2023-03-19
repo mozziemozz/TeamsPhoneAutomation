1f237a6305dc6405f1c154dfcc5a03a2232f0bd1
martin heusser <50239556+mozziemozz@users.noreply.github.com>
2023-03-19 22:13:30 +0100

    setup.ps1

--- Setup/Setup.ps1
+++ Setup/Setup.ps1
@@ -1,3 +1,5 @@
+Set-Location C:\Temp\GitHub\TeamsPhoneAutomation
+
 $TenantId = Get-Content -Path .\.local\TenantId.txt
 $automationAccountName = Get-Content -Path .\.local\AutomationAccountName.txt
 $resourceGroupName = Get-Content -Path .\.local\ResourceGroupName.txt
@@ -9,7 +11,8 @@ $installedModules = Get-InstalledModule
 
 $requiredModules = @(
 
-    "Az.Automation"
+    "Az.Automation",
+    "Az.Network"
 
 )
 
@@ -25,10 +28,45 @@ foreach ($module in $requiredModules) {
 
     else {
 
+        Write-Host "$module is not installed." -ForegroundColor Yellow
+
         $missingModules += $module
 
     }
 
+}
+
+if ($missingModules) {
+
+    function Test-Admin {
+      $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+      $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    }
+    
+    if ((Test-Admin) -eq $false)  {
+        if ($elevated) 
+        {
+            # tried to elevate, did not work, aborting
+        } 
+        else {
+            Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    
+        exit
+    }
+    
+    foreach ($module in $missingModules) {
+
+        Write-Host "Attempting to install $module..." -ForegroundColor Cyan
+
+        Install-Module -Name $module
+
+    }
+
+    Write-Host "Finished installing required modules. Restatarting script now..." -ForegroundColor Cyan
+
+    . $myinvocation.MyCommand.Definition
+    
 }
 
 # Connect to Azure Account
@@ -58,6 +96,17 @@ $functionRunbooks = Get-ChildItem -Path .\Functions
 
 foreach ($functionRunbook in $functionRunbooks) {
 
-    Import-AzAutomationRunbook -Path $functionRunbook.FullName -Tags $functionRunbookTags -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Type PowerShell -Name $($functionRunbook.Name.Replace(".ps1","")) -Published
+    $newRunbook = Import-AzAutomationRunbook -Path $functionRunbook.FullName -Tags $functionRunbookTags -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Type PowerShell -Name $($functionRunbook.Name.Replace(".ps1","")) -Published
+
+    if ($functionRunbook.Name -eq "Get-CsOnlineNumbers.ps1") {
+
+        $nextFullHour = (Get-Date).Hour
+        $startTime = (Get-Date "$($nextFullHour):00:00").ToUniversalTime().AddHours(2)
+
+        $newSchedule = New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $functionRunbook.Name.Replace(".ps1","") -StartTime $StartTime -HourInterval 1 -ResourceGroupName $resourceGroupName -TimeZone "Etc/UTC"
+
+        Register-AzAutomationScheduledRunbook -RunbookName $newRunbook.Name -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ScheduleName $newSchedule.Name
+
+    }
 
 }
