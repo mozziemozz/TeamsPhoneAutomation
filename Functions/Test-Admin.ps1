46801bba7e6d7e37980dbe22c0a1201d308d2ccd
martin heusser <50239556+mozziemozz@users.noreply.github.com>
2023-03-21 00:21:07 +0100

    setup.ps1

---
+++ Functions/Test-Admin.ps1
@@ -0,0 +1,4 @@
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
--- Setup/Setup.ps1
+++ Setup/Setup.ps1
@@ -1,5 +1,7 @@
 Set-Location C:\Temp\GitHub\TeamsPhoneAutomation
 
+. .\Functions\Test-Admin.ps1
+
 $TenantId = Get-Content -Path .\.local\TenantId.txt
 $automationAccountName = Get-Content -Path .\.local\AutomationAccountName.txt
 $resourceGroupName = Get-Content -Path .\.local\ResourceGroupName.txt
@@ -11,8 +13,7 @@ $installedModules = Get-InstalledModule
 
 $requiredModules = @(
 
-    "Az.Automation",
-    "Az.Network"
+    "Az.Automation"
 
 )
 
@@ -38,11 +39,6 @@ foreach ($module in $requiredModules) {
 
 if ($missingModules) {
 
-    function Test-Admin {
-      $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-      $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-    }
-    
     if ((Test-Admin) -eq $false)  {
         if ($elevated) 
         {
@@ -68,6 +64,150 @@ if ($missingModules) {
     . $myinvocation.MyCommand.Definition
     
 }
+
+# Check if Node.JS is installed
+
+try {
+
+    $ErrorActionPreference = "SilentlyContinue"
+    $checkNPM = npm list -g
+    $ErrorActionPreference = "Continue"
+
+    if ($checkNPM) {
+
+        Write-Host "Node.JS is already installed." -ForegroundColor Green
+
+        if ($checkNPM -match "@pnp/cli-microsoft365") {
+
+            Write-Host "CLI for Microsoft 365 is already installed." -ForegroundColor Green
+
+        }
+
+        else {
+
+            Write-Host "CLI for Microsoft 365 is not installed." -ForegroundColor Yellow
+            Write-Host "Attempting to install CLI for Microsoft 365..." -ForegroundColor Cyan
+
+            npm i -g @pnp/cli-microsoft365
+
+            if ($?) {
+
+                Write-Host "Finished installing CLI for Microsoft 365. Restatarting script now..." -ForegroundColor Cyan
+
+                . $myinvocation.MyCommand.Definition        
+
+            }
+
+            else {
+
+                Write-Error -Message "Error installing CLI for Microsoft 365. Please install it manually."
+
+            }
+
+        }
+
+    }
+
+}
+catch {
+
+    # Install Node.JS
+
+    Write-Host "Node.JS is not installed." -ForegroundColor Yellow
+    Write-Host "Attempting to install Node.JS..." -ForegroundColor Cyan
+
+    winget install --id=OpenJS.NodeJS  -e
+
+    if ($?) {
+
+        Write-Host "Finished installing Node.JS. Restatarting script now..." -ForegroundColor Cyan
+
+        # Reload path environment variable
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+        . $myinvocation.MyCommand.Definition
+
+    }
+
+    else {
+
+        Write-Error -Message "Error installing Node.JS. Please install it manually."
+
+    }
+
+}
+
+# Login to M365 CLI
+m365 login
+
+$AADAppRegistrationName = "$automationAccountName" + "_AAD_SP"
+
+$newAADApp = m365 aad app add --name $AADAppRegistrationName --withSecret
+
+$encryptedClientSecret = ConvertTo-SecureString ($newAADApp | ConvertFrom-Json).secrets.value -AsPlainText -Force
+
+Set-Content -Path .\.local\AppSecret.txt -Value ($encryptedClientSecret | ConvertFrom-SecureString) -Force
+
+Write-Host "Client secret saved to \.local\AppSecret.txt in an encrypted state." -ForegroundColor Cyan
+
+$reviewClientSecret = Read-Host "Would you like to display the Client secret? [Y] / [N] ?"
+
+switch ($reviewClientSecret) {
+    y {
+
+        Write-Host "Here's your Client secret. It has been decrypted using this machine and user $($env:USERNAME). The client secret can only be decrypted on this machine with this account!" -ForegroundColor Cyan
+        
+        $importedClientSecret = Get-Content -Path .\.local\AppSecret.txt | ConvertTo-SecureString
+
+        [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($importedClientSecret))
+
+        Write-Host "Script will continue in 15s. Please copy the secret and store it in a safe place." -ForegroundColor Yellow
+        Start-Sleep 15
+
+    }
+    n {
+        
+        Write-Host "Client secret won't be displayed." -ForegroundColor Magenta
+
+    }
+    Default {}
+}
+
+$newAppObjectId = (($newAADApp | ConvertFrom-Json).objectId).Trim()
+$newAppId = (($newAADApp | ConvertFrom-Json).appId).Trim()
+
+Write-Host "Adding the required Microsoft Graph permissions to $AADAppRegistrationName..." -ForegroundColor Cyan
+
+$newAADServicePrincipalObjectId = (m365 aad sp add --appId $newAppId | ConvertFrom-Json).Id
+
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "Organization.Read.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "User.Read.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "Channel.Delete.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "ChannelSettings.ReadWrite.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "Group.ReadWrite.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "ChannelMember.ReadWrite.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "AppCatalog.ReadWrite.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "TeamSettings.ReadWrite.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "Sites.Manage.All" --appId $newAppId
+m365 aad approleassignment add --resource "Microsoft Graph" --scope "RoleManagement.ReadWrite.Directory" --appId $newAppId
+
+. .\Functions\Connect-MgGraphHTTP.ps1
+
+$TenantId = Get-Content -Path .\.local\TenantId.txt
+$AppId = Get-Content -Path .\.local\AppId.txt
+$AppSecret = Get-Content -Path .\.local\AppSecret.txt
+
+. Connect-MgGraphHTTP -TenantId $TenantId -AppId $AppId -AppSecret $AppSecret
+
+$directoryRoleId = ((Invoke-RestMethod -Method Get -Headers $Header -Uri "https://graph.microsoft.com/v1.0/directoryRoles/").value | Where-Object {$_.displayName -eq "Skype for Business Administrator"}).Id
+
+$addDirectoryRoleBody = @"
+{
+    "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/$newAADServicePrincipalObjectId"
+}
+"@
+
+$addSfBAdminRole = Invoke-RestMethod -Method Post -Headers $Header -ContentType "application/json" -Uri "https://graph.microsoft.com/v1.0/directoryRoles/$directoryRoleId/members/`$ref" -Body $addDirectoryRoleBody
 
 # Connect to Azure Account
 $checkAzureSession = Get-AzContext > $null
