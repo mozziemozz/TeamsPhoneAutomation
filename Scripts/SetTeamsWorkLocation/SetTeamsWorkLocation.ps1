db7cc845d0523a27eb26f2f4e7c0065871e7d002
martin heusser <50239556+mozziemozz@users.noreply.github.com>
2023-07-08 00:43:17 +0200

    SetTeamsWorkLocation

--- Scripts/SetTeamsWorkLocation/SetTeamsWorkLocation.ps1
+++ Scripts/SetTeamsWorkLocation/SetTeamsWorkLocation.ps1
@@ -1,13 +1,60 @@
+<#
+    .SYNOPSIS
+    Script to set work location in Microsoft Teams.
+
+    .DESCRIPTION
+    Author:             Martin Heusser
+    Version:            1.0.1
+    Sponsor Project:    https://github.com/sponsors/mozziemozz
+    Website:            https://heusser.pro
+
+    This script sets the work location in Microsoft Teams based on the provided IP address and IP type (Local or Public). It requires the AADInternals module and external functions from SecureCredsMgmt.ps1.
+
+    .PARAMETER AdminUser
+    Specifies the admin user. If not provided, the current username is used.
+
+    .PARAMETER MFA
+    Indicates whether multi-factor authentication (MFA) is enabled.
+
+    .PARAMETER IpType
+    Specifies the IP type. Valid values are 'Local' and 'Public'.
+
+    .PARAMETER IpAddress
+    Specifies the IP address of your home/remote network.
+
+    .EXAMPLE
+    .\Set-TeamsWorkLocation.ps1 -AdminUser "user@domain.com" -MFA -IpType "Local" -IpAddress "192.168.1.10"
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]
+    $AdminUser = $env:USERNAME,
+
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $MFA,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Local', 'Public')]
+    [string]
+    $IpType,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $IpAddress
+)
+
 #requires -Modules "AADInternals"
 
 # Import external functions
 . .\Modules\SecureCredsMgmt.ps1
 
-. Get-MZZSecureCreds -AdminUser ""
-. Get-MZZSecureCreds -FileName "otpsecret"
+. Get-MZZSecureCreds -AdminUser $AdminUser
 
 $functions = Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.0" -Filter "*.ps1"
-# $functions = $functions | Where-Object {$_.Name -notmatch "AADSyncSettings"}
 
 $functions = $functions | Where-Object {$_.Name -match "Teams" -or $_.Name -match "AccessToken" -or $_.Name -match "CommonUtils"}
 
@@ -17,32 +64,75 @@ foreach ($function in $functions) {
 
 }
 
-# Teams Client
+# Teams Client Application Id
 $ClientId = "1fec8e78-bce4-4aaf-ab1b-5451cc387264"
 
-$teamsPresenceToken = Prompt-Credentials -ClientId $ClientId -Resource "https://presence.teams.microsoft.com" -Credentials $secureCreds -OTPSecretKey $passwordDecrypted
-# $teamsPresenceToken = Prompt-Credentials -ClientId $ClientId -Resource "https://api.spaces.skype.com" -Credentials $credentials -OTPSecretKey ""
+if ($MFA) {
 
+    . Get-MZZSecureCreds -FileName "otpsecret"
 
-$accessToken = $teamsPresenceToken.access_token
-
-$Header = @{Authorization = "Bearer $accessToken"}
-
-$ipInfo = Invoke-RestMethod -Method Get -Uri "https://ipapi.co/json"
-
-if ($ipInfo.ip.StartsWith("87.241.34.")) {
-
-    $workLocation = 1
-    $workLocationName = "Office"
+    $teamsPresenceToken = Prompt-Credentials -ClientId $ClientId -Resource "https://presence.teams.microsoft.com" -Credentials $secureCreds -OTPSecretKey $passwordDecrypted
 
 }
 
 else {
 
-    $workLocation = 2
-    $workLocationName = "Remote"
+    # Alternative resource:
+    # $teamsPresenceToken = Prompt-Credentials -ClientId $ClientId -Resource "https://api.spaces.skype.com" -Credentials $credentials
+
+    $teamsPresenceToken = Prompt-Credentials -ClientId $ClientId -Resource "https://presence.teams.microsoft.com" -Credentials $secureCreds
 
 }
+
+$accessToken = $teamsPresenceToken.access_token
+
+$Header = @{Authorization = "Bearer $accessToken"}
+
+switch ($IpType) {
+    Local {
+
+        $netAdapter = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}).Name
+        $netAddress = (Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq $netAdapter -and $_.AddressFamily -eq "IPv4"}).IPAddress
+
+        $netAddressMatch = $netAddress.Split(".")[0] + "." + $netAddress.Split(".")[1] + "." + $netAddress.Split(".")[2]
+
+        if ($IpAddress.StartsWith($netAddress)) {
+
+            $workLocation = 2
+            $workLocationName = "Remote"
+
+        }
+
+        else {
+
+            $workLocation = 1
+            $workLocationName = "Office"
+
+        }
+
+    }
+    Public {
+
+        $ipInfo = Invoke-RestMethod -Method Get -Uri "https://ipapi.co/json"
+
+        if ($ipInfo.ip.StartsWith("$IpAddress")) {
+
+            $workLocation = 2
+            $workLocationName = "Remote"
+
+        }
+
+        else {
+
+            $workLocation = 1
+            $workLocationName = "Office"
+
+        }
+
+    }
+    Default {}
+}
+
 
 $endOfDay = ((Get-Date).ToUniversalTime().Date.AddDays(1).AddSeconds(-1).GetDateTimeFormats() | Where-Object {$_ -match "GMT"})[-1]
 
