413e312a37509db0a191f1176564913ea6e38f55
Martin Heusser <50239556+mozziemozz@users.noreply.github.com>
2023-08-10 13:02:38 +0200

    AddMissingEmergencyLocationsToUnassignedNumbers

--- Scripts/EmergencyAdresses/AddMissingEmergencyLocationsToUnassignedNumbers.ps1
+++ Scripts/EmergencyAdresses/AddMissingEmergencyLocationsToUnassignedNumbers.ps1
@@ -1,3 +1,24 @@
+<#
+
+.SYNOPSIS
+    This script sets emergency locations for unassigned phone numbers in Microsoft Teams.
+
+.DESCRIPTION
+    This script connects to the Microsoft Teams service using a service principal and sets
+    emergency locations for phone numbers that do not have a location assigned.
+
+.NOTES
+    Author:             Martin Heusser
+    Version:            1.0.0
+    Prerequisites:      PowerShell with MicrosoftTeams module version 5.4.0,
+                        Entra ID service principal with Teams Administrator rights
+    Sponsor Project:    https://github.com/sponsors/mozziemozz
+    Website:            https://heusser.pro
+    Link:               https://github.com/mozziemozz/TeamsPhoneAutomation/blob/main/Scripts/EmergencyAdresses/AddMissingEmergencyLocationsToUnassignedNumbers.ps1
+
+#>
+
+# Requires minimum version of the MicrosoftTeams module
 #Requires -Modules @{ ModuleName = "MicrosoftTeams"; ModuleVersion = "5.4.0" }
 
 # Import external functions
@@ -5,33 +26,44 @@
 . .\Functions\Get-CsOnlineNumbers.ps1
 . .\Modules\SecureCredsMgmt.ps1
 
+# Get tenant ID and application ID from configuration files
 . Get-MZZTenantIdTxt
 . Get-MZZAppIdTxt
 
+# Get secure credentials and decrypt the app secret
 . Get-MZZSecureCreds -FileName AppSecret
 $AppSecret = $passwordDecrypted
 
+# Connect to Microsoft Teams service principal
 . Connect-MsTeamsServicePrincipal -TenantId $TenantId -AppId $AppId -AppSecret $AppSecret
 
 # Get CsOnline Numbers
 $allCsOnlineNumbers = . Get-CsOnlineNumbers
 
-$numbersWithoutEmergencyLocation = $allCsOnlineNumbers | Where-Object {!$_.LocationId -and $_.Capability -notcontains "ConferenceAssignment" -and $_.City -ne "Toll-Free" -and $_.LocationUpdateSupported -eq $true}
+# Filter phone numbers without emergency location
+$numbersWithoutEmergencyLocation = $allCsOnlineNumbers | Where-Object {!$_.LocationId -and $_.LocationUpdateSupported -eq $true}
 
+# Iterate through numbers without emergency location
 foreach ($number in $numbersWithoutEmergencyLocation) {
-
     Write-Host "Setting emergency location for number $number..." -ForegroundColor Cyan
 
-    $matchingEmergencyLocationId = ($allCsOnlineNumbers | Where-Object {$_.City -eq $number.City -and $_.LocationId -ne $null}).LocationId[0]
+    # Find matching emergency location ID
+    $matchingEmergencyLocationId = ($allCsOnlineNumbers | Where-Object {$_.City -eq $number.City -and $_.LocationId -ne $null}).LocationId
+
+    if ($matchingEmergencyLocationId.Count -gt 1) {
+
+        $matchingEmergencyLocationId = $matchingEmergencyLocationId[0]
+
+    }
 
     if ($matchingEmergencyLocationId) {
 
         Write-Host "Setting Location Id '$matchingEmergencyLocationId' for number $($number.TelephoneNumber)..."
-
+        
         Set-CsPhoneNumberAssignment -PhoneNumber $number.TelephoneNumber -LocationId $matchingEmergencyLocationId
 
-    }
-
+    } 
+    
     else {
 
         Write-Host "No existing Location Id found for number $($number.Telephonenumber)..."
@@ -39,4 +71,3 @@ foreach ($number in $numbersWithoutEmergencyLocation) {
     }
 
 }
-
