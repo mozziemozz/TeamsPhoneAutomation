810771de92633fb70291864029ea07868be4ecc4
Martin Heusser <50239556+mozziemozz@users.noreply.github.com>
2023-10-25 16:05:01 +0200

    TeamsClipboardDialer 1.0.5

--- TeamsClipboardDialer/TeamsClipboardDialer.ps1
+++ TeamsClipboardDialer/TeamsClipboardDialer.ps1
@@ -9,12 +9,15 @@
 
     .NOTES
     Author:             Martin Heusser | M365 Apps & Services MVP
-    Version:            1.0.4
+    Version:            1.0.5
     Changes:            2023-10-24
                         Add hint if clipboard is empty
                         Improve regex matching, add code to change 00 to +
                         Fix 00 to + replacement, add comments
                         Switch to BalloonTip instead of MessageBox for invalid clipboard content
+                        
+                        2023-10-25
+                        Improve regular expressions, improve message hints
     Sponsor Project:    https://github.com/sponsors/mozziemozz
     Website:            https://heusser.pro
 
@@ -30,9 +33,20 @@
 
 #>
 
-$phoneNumber = Get-Clipboard | Out-String
+# Get clipboard content
+$clipboard = Get-Clipboard
 
+# Convert clipboard content to string
+$phoneNumber = $clipboard | Out-String
+
+# Save the original clipboard value
 $originalClipboardValue = $phoneNumber.Trim()
+
+# Count the number of digits in the clipboard without whitespaces
+$digitCount = ($originalClipboardValue -replace '[^\d\s]').Length
+
+# Count the number of non digits in the clipboard
+$nonDigitCount = ($originalClipboardValue -replace '[\d\s]').Length
 
 # Trim eventual whitespaces
 $phoneNumber = $phoneNumber.Trim()
@@ -43,12 +57,19 @@ $phoneNumber = $phoneNumber -replace '[^\d\+]'
 # Replace leading 00 with +
 $phoneNumber = $phoneNumber -replace '^00', '+'
 
+# Add + if the number doesn't start with 0 or + and is at least 5 digits long
+if (!$phoneNumber.StartsWith("0") -and !$phoneNumber.StartsWith("+") -and $phoneNumber.Length -ge 5) {
+
+    $phoneNumber = "+$phoneNumber"
+
+}
+
 # Remove any invalid zeros
 # Credits: https://ucken.blogspot.com/2016/03/trunk-prefixes-in-skype4b.html
 $phoneNumber = $phoneNumber -replace ('^\+(1|7|2[07]|3[0-46]|39\d|4[013-9]|5[1-8]|6[0-6]|8[1246]|9[0-58]|2[1235689]\d|24[013-9]|242\d|3[578]\d|42|5[09]\d|6[789]\d|8[035789]\d|9[679]\d)(?:0)?(\d{6,14})?$', '+$1$2')
 
 # Check if there is a phone number in the clipboard
-if ($phoneNumber -notmatch '^(?:\+\d+|\d+)') {
+if ($phoneNumber -notmatch '^(\d{2,}|(\+\d{5,}))$' -or $nonDigitCount -ge $digitCount) {
 
     if ($originalClipboardValue -eq "SetUpTeamsClipboardDialer") {
 
@@ -66,24 +87,33 @@ if ($phoneNumber -notmatch '^(?:\+\d+|\d+)') {
         # Check if $phoneNumber is null or whitespace
         if (!$originalClipboardValue) {
 
-            $clipboardContent = "Clipboard is empty."
+            $messageClipboardContent = "Clipboard is empty."
 
         }
 
         else {
 
-            # Get the clipboard content
-            $clipboardContent = $originalClipboardValue
+            if ($clipboard.GetType().BaseType.Name -eq "Array") {
 
+                $messageClipboardContent = "Clipboard contains multiple lines."
+
+            }
+
+            else {
+
+                # Set to clipboard content
+                $messageClipboardContent = $clipboard
+
+            }
+            
         }
 
-        # $Message = "Clipboard doesn't contain a phone number. Copy a phone number and try again. Clipboard content: $clipboardContent"
+        # $Message = "Clipboard doesn't contain a phone number. Copy a phone number and try again. Clipboard content: $messageClipboardContent"
         $Message = @"
 Clipboard doesn't contain a phone number. 
 Copy a phone number and try again. 
 
-Clipboard content: $clipboardContent
-
+Clipboard content: $messageClipboardContent
 "@
         $Title = "Teams Clipboard Dialer"
 
